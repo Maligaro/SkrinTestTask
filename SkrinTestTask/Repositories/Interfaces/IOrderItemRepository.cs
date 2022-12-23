0570adca5ec8f78254613e6e6a8975cae5dfc8e9
Maligaro <skopin-nik@mail.ru>
2022-12-23 14:11:12 +0500

    Add orderItem repository

---
+++ SkrinTestTask/Repositories/Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using SkrinTestTask.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrinTestTask.Repositories.Interfaces
+{
+    public interface IOrderItemRepository
+    {
+        public void CreateOrderItem(OrderItem orderItem);
+        public OrderItem GetOrderItemByOrderId(int orderId);
+        public OrderItem GetOrderItemByProductId(int productId);
+    }
+}
---
+++ SkrinTestTask/Repositories/OrderItemRepository.cs
@@ -0,0 +1,44 @@
+﻿using SkrinTestTask.Model.Entities;
+using SkrinTestTask.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrinTestTask.Repositories
+{
+    public class OrderItemRepository : IOrderItemRepository
+    {
+        public void CreateOrderItem(OrderItem orderItem)
+        {
+            using (var db = new ApplicationContext())
+            {
+                db.OrderItems.Add(orderItem);
+                db.SaveChanges();
+            }
+        }
+
+        public OrderItem GetOrderItemByOrderId(int orderId)
+        {
+            using (var db = new ApplicationContext())
+            {
+                var orderItem = db.OrderItems.FirstOrDefault(oi => oi.Order.Id == orderId);
+                if (orderItem is null)
+                    throw new Exception($"There is no order item with orderId = \"{orderId}\" in the database");
+                return orderItem;
+            }
+        }
+
+        public OrderItem GetOrderItemByProductId(int productId)
+        {
+            using (var db = new ApplicationContext())
+            {
+                var orderItem = db.OrderItems.FirstOrDefault(oi => oi.Product.Id == productId);
+                if (orderItem is null)
+                    throw new Exception($"There is no order item with productId = \"{productId}\" in the database");
+                return orderItem;
+            }
+        }
+    }
+}
